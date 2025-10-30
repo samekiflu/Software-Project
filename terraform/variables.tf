dc2da2accbdef36a052bd515da4f516516978ba0
Ihenna Anoliefo <ianolief@purdue.edu>
2025-10-30 17:39:49 -0400

    Add AWS region, environment, and project variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,15 @@
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "env" {
+  type    = string
+  default = "dev"
+}
+
+variable "project" {
+  type    = string
+  default = "SoftwareProject"
+}
 
