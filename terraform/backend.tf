6108dc844e3b2e3f09a806194fcaa2139df6d0ab
Ihenna Anoliefo <ianolief@purdue.edu>
2025-10-30 17:15:09 -0400

    Set up S3 backend in backend.tf
    Configured S3 backend for Terraform state management.

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,1 +1,10 @@
+terraform {
+  backend "s3" {
+    bucket         = "software-project-terraform-state"   # create this bucket in AWS first
+    key            = "dev/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "software-project-tf-locks"          # create this table in AWS first
+    encrypt        = true
+  }
+}
 
