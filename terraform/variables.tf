1f0d386abf0833dc16fa53ec5d45a223834151d0
Ihenna Anoliefo <ianolief@purdue.edu>
2025-10-30 17:59:27 -0400

    Refactor variables in variables.tf
    Consolidate variable definitions and add descriptions for tokens.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,15 +1,15 @@
-variable "aws_region" {
-  type    = string
-  default = "us-east-1"
+variable "aws_region" { default = "us-east-1" }
+variable "project"    { default = "software-project" }
+variable "env"        { default = "dev" }
+
+variable "github_token" {
+  description = "GitHub Personal Access Token"
+  type        = string
+  default     = ""
 }
 
-variable "env" {
-  type    = string
-  default = "dev"
+variable "hf_api_token" {
+  description = "Hugging Face Token"
+  type        = string
+  default     = ""
 }
-
-variable "project" {
-  type    = string
-  default = "SoftwareProject"
-}
-
