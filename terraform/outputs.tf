1c6627ff7d59d5ce521f46904317875e751bbd0d
Ihenna Anoliefo <ianolief@purdue.edu>
2025-10-30 17:16:16 -0400

    Add outputs for Cognito, SNS, and CloudFront

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,1 +1,13 @@
+output "cognito_user_pool_id" {
+  value = aws_cognito_user_pool.users.id
+}
+output "cognito_client_id" {
+  value = aws_cognito_user_pool_client.app.id
+}
+output "sns_topic_arn" {
+  value = aws_sns_topic.alerts.arn
+}
+output "frontend_url" {
+  value = aws_cloudfront_distribution.frontend.domain_name
+}
 
